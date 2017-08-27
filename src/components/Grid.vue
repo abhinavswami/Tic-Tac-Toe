f19e360de79e9846fd265a5129eb31b65522f4bb
Abhinav Swami <abhinav.swami21@gmail.com>
2017-08-26 18:21:57 -0700

    Add properties

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -20,7 +20,36 @@
 
 <script>
 export default {
-    data() { }
+    data() {
+        return {
+            // can be X or O
+            activePlayer: 'O',
+            // maintain the status of the game - win, draw or turn
+            gameStatus: 'turn',
+            gameStatusMessage: `O's turn`,
+            // status color is used as the background color in the status bar
+            // it can hold the name of the either of the following classes
+            // statusTurn (default) is yellow for a turn
+            // statusWin is green for a win
+            // statusDraw is purple for a draw
+            gameStatusColor: 'statusTurn',
+            // no of moves played by both players in a sinlge game (max=9)
+            moves: 0,
+            // store the placement of X and O in the cells y their cell number
+            cells: {
+                1: '', 2: '', 3: '',
+                4: '', 5: '', 6: '',
+                7: '', 8: '', 9: '',
+            },
+            // contains all 8 possible winning conditions 
+            windConditions: [
+                [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+                [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+                [1, 5, 9], [3, 5, 7]             // diagonals
+            ],
+
+        }
+    }
 }
 </script>
 
